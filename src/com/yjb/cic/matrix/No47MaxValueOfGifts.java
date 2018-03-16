59e57e43b20a7d6dee59aaa677a56e801ab6ad2e
yangjiabing <yangjiabing1994@outlook.com>
2018-03-17 00:57:36 +0800

    Add No47MaxValueOfGifts.

---
+++ src/com/yjb/cic/matrix/No47MaxValueOfGifts.java
@@ -0,0 +1,26 @@
+package com.yjb.cic.matrix;
+
+/**
+ * 面试题47：礼物的最大价值
+ * 题目：在一个m×n的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值
+ * （价值大于0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向左或
+ * 者向下移动一格直到到达棋盘的右下角。给定一个棋盘及其上面的礼物，请计
+ * 算你最多能拿到多少价值的礼物？
+ */
+public class No47MaxValueOfGifts {
+
+    private static int getMaxValue(int[][] values) {
+        if (values == null || values.length == 0 || values[0].length == 0) {
+            return 0;
+        }
+        int cols = values[0].length;
+        int[] dp = new int[cols];
+        for (int[] row : values) {
+            dp[0] += row[0];
+            for (int j = 1; j < cols; j++) {
+                dp[j] = Math.max(dp[j], dp[j - 1]) + row[j];
+            }
+        }
+        return dp[cols - 1];
+    }
+}
