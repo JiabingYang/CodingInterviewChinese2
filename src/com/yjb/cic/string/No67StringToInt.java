31c340549352a258da9acbc6f312b59dec201c43
yangjiabing <yangjiabing1994@outlook.com>
2018-03-22 21:56:00 +0800

    Add some comments to No67StringToInt.

--- src/com/yjb/cic/string/No67StringToInt.java
+++ src/com/yjb/cic/string/No67StringToInt.java
@@ -4,6 +4,8 @@ package com.yjb.cic.string;
  * 面试题67：把字符串转换成整数
  * 题目：请你写一个函数StrToInt，实现把字符串转换成整数这个功能。当然，不
  * 能使用atoi或者其他类似的库函数。
+ * <p>
+ * Leetcode: 8. String to Integer (atoi)
  */
 public class No67StringToInt {
 
