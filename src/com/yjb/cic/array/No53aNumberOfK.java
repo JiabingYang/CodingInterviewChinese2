f413381d2904283ae7444a1d32e9170ded95f1a4
yangjiabing <yangjiabing1994@outlook.com>
2018-03-23 11:01:45 +0800

    Optimize the solution for No53aNumberOfK.

--- src/com/yjb/cic/array/No53aNumberOfK.java
+++ src/com/yjb/cic/array/No53aNumberOfK.java
@@ -6,6 +6,8 @@ import java.util.Arrays;
  * 面试题53（一）：数字在排序数组中出现的次数
  * 题目：统计一个数字在排序数组中出现的次数。例如输入排序数组{1, 2, 3, 3,
  * 3, 3, 4, 5}和数字3，由于3在这个数组中出现了4次，因此输出4。
+ * <p>
+ * Leetcode: 34. Search for a Range
  */
 public class No53aNumberOfK {
 
@@ -25,45 +27,46 @@ public class No53aNumberOfK {
 
     private static void test(int[] nums, int k, int result) {
         System.out.println("[" + Arrays.toString(nums) + ", "
-                + k + ", " + result + "] " + (result == getNumberOfK(nums, k)));
+                + k + ", " + result + "] " + (result == mySolution(nums, k)));
     }
 
-    private static int getNumberOfK(int[] nums, int k) {
+    /**
+     * 对书本上写法做了优化
+     */
+    private static int mySolution(int[] nums, int k) {
         if (nums == null || nums.length == 0) {
             return 0;
         }
-        int first = getFirstK(nums, k);
-        if (first == -1) {
+        int left = findLeft(nums, k, 0, nums.length - 1);
+        if (left == -1) {
             return 0;
         }
-        return getLastK(nums, k) - first + 1;
+        return findRight(nums, k, left, nums.length - 1) - left + 1;
     }
 
-    private static int getFirstK(int[] nums, int k) {
-        int l = 0;
-        int r = nums.length - 1;
+    private static int findLeft(int[] nums, int target, int l, int r) {
+        int temp = r;
         while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] < k) {
+            int mid = l + ((r - l) >>> 1);
+            if (nums[mid] < target) {
                 l = mid + 1;
             } else {
                 r = mid - 1;
             }
         }
-        return (l < nums.length && nums[l] == k) ? l : -1;
+        return (l <= temp && nums[l] == target) ? l : -1;
     }
 
-    private static int getLastK(int[] nums, int k) {
-        int l = 0;
-        int r = nums.length - 1;
+    private static int findRight(int[] nums, int target, int l, int r) {
+        int temp = l;
         while (l <= r) {
-            int mid = l + (r - l) / 2;
-            if (nums[mid] > k) {
+            int mid = l + ((r - l) >>> 1);
+            if (nums[mid] > target) {
                 r = mid - 1;
             } else {
                 l = mid + 1;
             }
         }
-        return (r >= 0 && nums[r] == k) ? r : -1;
+        return (r >= temp && nums[r] == target) ? r : -1;
     }
 }
